89e351b6c5252b5937fca72a7806129da9a23be2
Peritract <dan@getfutureproof.co.uk>
2023-01-31 13:58:11 +0000

    mvc

---
+++ README.md
@@ -0,0 +1,23 @@
+# Architecture
+
+A project about the savage mushroom-people who live in the caverns below the city.
+
+![mushroom people](./assets/mushroom-people.jpg)
+
+## Installation
+
+`npm run install -D`
+
+## Development server
+
+`npm run dev`
+
+## Deployment server
+
+`npm run start`
+
+## Notes
+
+### Model-View-Controller (MVC)
+
+### RESTful APIs
---
+++ database/mushrooms.js
@@ -0,0 +1,46 @@
+const mushrooms = [
+    {
+        id: 1,
+        name: "Mordak",
+        species: "myconid",
+        age: 3,
+        role: "warrior"
+    },
+    {
+        id: 2,
+        name: "Umdar",
+        species: "myconid",
+        age: 1,
+        role: "gatherer"
+    },
+    {
+        id: 3,
+        name: "Stresna",
+        species: "myconid",
+        age: 4,
+        role: "chieftain"
+    },
+    {
+        id: 4,
+        name: "Vircon",
+        species: "myconid",
+        age: 7,
+        role: "shaman"
+    },
+    {
+        id: 5,
+        name: "Relno",
+        species: "myconid",
+        age: 1,
+        role: "gatherer"
+    },
+    {
+        id: 6,
+        name: "Noldu",
+        species: "myconid",
+        age: 3,
+        role: "sporesmith"
+    }
+]
+
+module.exports = mushrooms;
